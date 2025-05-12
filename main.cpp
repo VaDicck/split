02cbf69e70ea0496c1abd1c6675e6c680c80e62a
VaDicck <vadiknadezhkin@yandex.ru>
2025-05-12 22:34:45 +0300

    first

--- main.cpp
+++ main.cpp
@@ -1,19 +1,56 @@
 #include <QCoreApplication>
+#include <argument.h>
+
+//Пропустить константу
+bool skipConstant(const QString &string, int &indexCurrentSimbol, const char &simbol);
+//Пропустить многострочный комментарий
+void skipMultilineComment(const QStringList &code,  int &indexCurrentString, int &indexCurrentSimbol);
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+}
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
-
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
-
-    return a.exec();
+//Пропустить константу
+bool skipConstant(const QString &string, int &indexCurrentSimbol, const char &simbol){
+    //Найти индекс следующей возможной константы в строке
+    int indexConstant = indexCurrentSimbol+ string.mid(indexCurrentSimbol).indexOf(simbol);
+    //Если была найденна
+    if(indexConstant != -1){
+        //Если перед парой константы нет экранирования
+        if(string[indexConstant-1] != '\\'){
+            //Считаем текущий индекс за второй парой
+            indexCurrentSimbol = indexConstant+1;
+            //Считать пару найденной
+            return true;
+        }
+        //Иначе
+        else{
+            //Считать, что экранирование одно
+            int countBackup = 1;
+            //Для каждого символа перед второй константой и до первой
+            for(int i =indexConstant-2;i>=indexCurrentSimbol;--i){
+                //Если сивол является экранированием увеличить счетчик количества экранирования
+                if(string[i] == '\\'){
+                    countBackup++;
+                }
+                //Иначе закончить поиск
+                else i =-1;
+            }
+            indexCurrentSimbol = indexConstant+1;
+            //Если количество нечетное
+            if(countBackup%2 != 0){
+                //Исследовать строку далее
+                return skipConstant(string, indexCurrentSimbol, simbol);
+            }
+            //Иначе нашли пару
+            else{
+                return true;
+            }
+        }
+    }
+    //Иначе
+    else{
+        //Считать пару не найденной
+        return false;
+    }
 }
