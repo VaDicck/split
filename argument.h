8236b11898bcebb493fe157bab33f466d04dbfab
VaDicck <vadiknadezhkin@yandex.ru>
2025-05-12 18:51:33 +0300

    nuul

---
+++ argument.h
@@ -0,0 +1,14 @@
+#ifndef ARGUMENT_H
+#define ARGUMENT_H
+#include <QString>
+
+class argument
+{
+private:
+    QString nameArgument;  // Имя аргумента
+    QString type;          // Тип аргумента
+public:
+    void setName();
+};
+
+#endif // ARGUMENT_H
