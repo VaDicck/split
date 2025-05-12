ed931e1b3363f4a16b3e82322703ee7afee2a689
VaDicck <vadiknadezhkin@yandex.ru>
2025-05-12 21:56:41 +0300

    first

--- argument.h
+++ argument.h
@@ -8,7 +8,10 @@ private:
     QString nameArgument;  // Имя аргумента
     QString type;          // Тип аргумента
 public:
-    void setName();
+    void setName(const QString& name); //Сеттер для имени
+    void setType(const QString& type); //Сеттер для типа
+    QString getName(); //Геттер для имени
+    QString getType(); //Геттер для типа
 };
 
 #endif // ARGUMENT_H
