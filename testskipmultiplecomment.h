f8bed3d1eb6f5905cf51ad85f91df11b7363dba2
VaDicck <vadiknadezhkin@yandex.ru>
2025-05-27 21:44:36 +0300

    first

--- testskipmultiplecomment.h
+++ testskipmultiplecomment.h
@@ -8,16 +8,8 @@ class testSkipMultipleComment : public QObject
 {
     Q_OBJECT
 private slots:
-    void inOneString(); //В одной строке
-    void inDifferentStrokes(); //В разных строках
-    void shielding(); //Экранирование
-    void multipleShielding(); //Множественное экранирование
-    void shieldingOfShieldingBeforeComment(); //Экранирование экранирования перед комменатрием
-    void notCompleted(); //Не завершен
-    void shieldingFinalComment(); //Экранирование завершающего комментария
-    void slashInsideComment(); //Слэш внутри комментария
-    void startCommentInternalComment(); //Начало комментария внутри комментария
-    void closingCommentaryCrossSpace(); //Завершающий комментарий через пробел
+    void add_data();
+    void add();
 };
 
 #endif // TESTSKIPMULTIPLECOMMENT_H
