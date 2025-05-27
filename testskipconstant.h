1bea99f325cfdcf7d2e83924b8aaf422af425c09
VaDicck <vadiknadezhkin@yandex.ru>
2025-05-27 21:43:09 +0300

    first

--- testskipconstant.h
+++ testskipconstant.h
@@ -8,17 +8,8 @@ class testSkipConstant : public QObject
 {
     Q_OBJECT
 private slots:
-    void symbolConstat(); //Символьная константа
-    void symbolConstantaShielding(); //Символьная константа экранирование
-    void symbolConstantaShieldingOfShielding(); //Символьная константа экранирование экранированиия
-    void stringConstant(); //Строковая константа
-    void stringConstantTwoShields(); //Строковая константа два экранирования
-    void stringConstantShieldingOfShielding(); //Строковая константа экранирования экранирования
-    void notCompletedSymbolConstant(); //Не завершена символьная константа
-    void notCompletedStringConstant(); //Не завершена строковая константа
-    void multipleShielding(); //Множественное экранирование
-    void symbolConstantIncludesStringConstant(); //Символьная константа включает строковую
-    void stringConstantIncludesSimbolConstant(); //Строковая константа включает символьную
+    void add_data();
+    void add();
 };
 
 #endif // TESTSKIPCONSTANT_H
