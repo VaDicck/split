5483f3f6f31075e781f88dea16421e85f2ebbb4e
VaDicck <vadiknadezhkin@yandex.ru>
2025-05-27 21:44:47 +0300

    first

--- testskipmultiplecomment.cpp
+++ testskipmultiplecomment.cpp
@@ -1,140 +1,64 @@
 #include "testskipmultiplecomment.h"
 #include "main.h"
+void testSkipMultipleComment::add_data()
+{
+    QTest::addColumn<QStringList>("code");
+    QTest::addColumn<int>("indexString");
+    QTest::addColumn<int>("indexStart");
+    QTest::addColumn<int>("exIndexString");
+    QTest::addColumn<int>("exIndex");
+    QTest::addColumn<bool>("exResult");
 
-void testSkipMultipleComment::inOneString(){
-    QStringList code ={"char br /*=‘a’*/;",
-                        "int a=7, b=4;"};
-    int indexString = 0;
-    int indexStart = 10;
-    int exIndexString = 0;
-    int exIndex = 16;
-    bool exResult = true;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
+    QTest::newRow("inOneString")
+        << QStringList{"char br /*=‘a’*/;", "int a=7, b=4;"}
+        << 0 << 10 << 0 << 16 << true;
+
+    QTest::newRow("inDifferentStrokes")
+        << QStringList{"char br/**= ‘a’  ;", "int a=7, b=4;", "int g();*/ int c;"}
+        << 0 << 9 << 2 << 10 << true;
+
+    QTest::newRow("shielding")
+        << QStringList{"char br/**= ‘a’\\*/;", "int a=7, b=4;", "int g();*/ int c;"}
+        << 0 << 9 << 2 << 10 << true;
+
+    QTest::newRow("multipleShielding")
+        << QStringList{"char br/**= ‘a’\\\\\\*/;", "int a=7, b=4;", "int g();*/ int c;"}
+        << 0 << 9 << 2 << 10 << true;
+
+    QTest::newRow("shieldingOfShieldingBeforeComment")
+        << QStringList{"char br/**= ‘a’   ;", "int a=7, b=4;", "int \\\\\\\\*/ int c;"}
+        << 0 << 9 << 2 << 10 << true;
+
+    QTest::newRow("notCompleted")
+        << QStringList{"char br/**= ‘a’   ;", "int a=7, b=4;", "int g();   int c;"}
+        << 0 << 9 << 0 << 9 << false;
+
+    QTest::newRow("shieldingFinalComment")
+        << QStringList{"char br/**= ‘a’   ;", "int a=7, b=4;", "int g(); \\*/  int c;"}
+        << 0 << 9 << 0 << 9 << false;
+
+    QTest::newRow("slashInsideComment")
+        << QStringList{"char br/**= ‘a’  ;", "int /a=7, /b=/4;", "int g();*/ int c;"}
+        << 0 << 9 << 2 << 10 << true;
+
+    QTest::newRow("startCommentInternalComment")
+        << QStringList{"char br/**= ‘a’  ;", "int a=7, /*b=/4;", "int/* g();*/ int c;"}
+        << 0 << 9 << 2 << 12 << true;
+
+    QTest::newRow("closingCommentaryCrossSpace")
+        << QStringList{"char br/**= ‘a’  ;", "int a* /=7, b=4;", "int *  /g();*/ int c;"}
+        << 0 << 9 << 2 << 14 << true;
 }
-void testSkipMultipleComment::inDifferentStrokes(){
-    QStringList code ={"char br/**= ‘a’  ;",
-        "int a=7, b=4;",
-        "int g();*/ int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 2;
-    int exIndex = 10;
-    bool exResult = true;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::shielding(){
-    QStringList code ={"char br/**= ‘a’\\*/;",
-        "int a=7, b=4;",
-        "int g();*/ int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 2;
-    int exIndex = 10;
-    bool exResult = true;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::multipleShielding(){
-    QStringList code ={"char br/**= ‘a’\\\\\\*/;",
-        "int a=7, b=4;",
-        "int g();*/ int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 2;
-    int exIndex = 10;
-    bool exResult = true;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::shieldingOfShieldingBeforeComment(){
-    QStringList code ={"char br/**= ‘a’   ;",
-                        "int a=7, b=4;",
-                        "int \\\\\\\\*/ int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 2;
-    int exIndex = 10;
-    bool exResult = true;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::notCompleted(){
-    QStringList code ={"char br/**= ‘a’   ;",
-        "int a=7, b=4;",
-        "int g();   int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 0;
-    int exIndex = 9;
-    bool exResult = false;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::shieldingFinalComment(){
-    QStringList code ={"char br/**= ‘a’   ;",
-        "int a=7, b=4;",
-        "int g(); \\*/  int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 0;
-    int exIndex = 9;
-    bool exResult = false;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::slashInsideComment(){
-    QStringList code ={"char br/**= ‘a’  ;",
-        "int /a=7, /b=/4;",
-        "int g();*/ int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 2;
-    int exIndex = 10;
-    bool exResult = true;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::startCommentInternalComment(){
-    QStringList code ={"char br/**= ‘a’  ;",
-        "int a=7, /*b=/4;",
-        "int/* g();*/ int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 2;
-    int exIndex = 12;
-    bool exResult = true;
-    bool result = skipMultilineComment(code, indexString, indexStart);
-    QCOMPARE(result, exResult);
-    QCOMPARE(indexString, exIndexString);
-    QCOMPARE(indexStart, exIndex);
-}
-void testSkipMultipleComment::closingCommentaryCrossSpace(){
-    QStringList code ={"char br/**= ‘a’  ;",
-        "int a* /=7, b=4;",
-        "int *  /g();*/ int c;"};
-    int indexString = 0;
-    int indexStart = 9;
-    int exIndexString = 2;
-    int exIndex = 14;
-    bool exResult = true;
+
+void testSkipMultipleComment::add()
+{
+    QFETCH(QStringList, code);
+    QFETCH(int, indexString);
+    QFETCH(int, indexStart);
+    QFETCH(int, exIndexString);
+    QFETCH(int, exIndex);
+    QFETCH(bool, exResult);
+
     bool result = skipMultilineComment(code, indexString, indexStart);
     QCOMPARE(result, exResult);
     QCOMPARE(indexString, exIndexString);
