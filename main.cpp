1d31308d82f9b09230dfde7170e9f160265d3226
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-01 18:07:26 +0300

    first

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@
 int main(int argc, char *argv[])
 {
     QTest::qExec(new testSkipConstant,argc,argv);
+    QTest::qExec(new testSkipMultipleComment,argc,argv);
 }
 
 //Пропустить константу
 bool skipConstant(const QString &string, int &indexCurrentSimbol, const char &simbol){
     //Если нет пары
     if(string.mid(indexCurrentSimbol).indexOf(simbol) == -1){
+        //Конец строки
+        indexCurrentSimbol = string.length();
         return false; //Считать что пары в строке нет
     }
     //Найти индекс второй пары
@@ -52,4 +55,71 @@ bool skipConstant(const QString &string, int &indexCurrentSimbol, const char &si
             return true;
         }
     }
+}
+
+//Пропустить многострочный комментарий
+bool skipMultilineComment(const QStringList &code,  int &indexCurrentString, int &indexCurrentSimbol){
+    // Запомнить начальные значения
+    int beginString = indexCurrentString, beginIndex = indexCurrentSimbol;
+    // Пока не кончится код
+    while(indexCurrentString<code.size()){
+        int indexComment = code[indexCurrentString].mid(indexCurrentSimbol).indexOf("*/");
+        // Если не был найден завершающий
+        if(indexComment == -1){
+            // Перейти на следующую строку
+            indexCurrentString++;
+            indexCurrentSimbol=0;
+        }
+        // Иначе
+        else{
+            // Индекс за комментария
+            indexCurrentSimbol = indexCurrentSimbol+ indexComment + 2;
+            // Закончить поиск
+            return true;
+        }
+    }
+    indexCurrentString = beginString, indexCurrentSimbol = beginIndex;
+    return false;
+}
+
+//Найти лексемы
+QPair<QString, QStringList> findLexemes(const QStringList &code, int &currentString, int &currentSimbol,const QStringList &neededLexemes,const QStringList &needSimbols, const QStringList &endLexemes)
+{
+
+}
+
+//Разбить пакет
+package_info* splitPackage(const QStringList &packageDeclaration, package_info &rootPackage) {
+
+}
+
+
+//Разбить импорт
+QString splitImport(const QStringList &declarationImport){
+
+}
+
+//Разбить метод
+method splitMethod(const QStringList &code, int &indexCurrentString, int &indexCurrentSimbol, const QString &nameClass, const QStringList &methodDeclaration, QSet<error> errors){
+
+}
+
+// Разбить интерфейс
+interface_info splitInterface(const QStringList &code, int &indexCurrentString, int &indexCurrentSimbol , const QStringList &interfaceDeclaration, QSet<error> errors){
+
+}
+
+// Разбить поле
+QMap<QString, field> splitField(const QStringList &fieldDeclaration){
+
+}
+
+// Разбить класс
+class_info splitClass(const QStringList &code, int &indexCurrentString, int &indexCurrentSimbol,const QStringList &declarationClass, QSet<error> errors){
+
+}
+
+// Разбить проект
+void splitProject(const QList<QStringList> &project, package_info &rootPack){
+
 }
