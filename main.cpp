b058991e3930caa7ec36e708235624d974d75c14
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-11 14:54:42 +0300

    third

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@ int main(int argc, char *argv[])
 {
     //QTest::qExec(new testSkipConstant,argc,argv);
     //QTest::qExec(new testSkipMultipleComment,argc,argv);
-    QTest::qExec(new testFindLexemes,argc,argv);
+    //QTest::qExec(new testFindLexemes,argc,argv);
+    //QTest::qExec(new testSplitField,argc,argv);
+    //QTest::qExec(new testsplitimport,argc,argv);
+    QTest::qExec(new testsplitmethod,argc,argv);
 }
 
 //Пропустить константу
@@ -175,12 +178,132 @@ package_info* splitPackage(const QStringList &packageDeclaration, package_info &
 
 //Разбить импорт
 QString splitImport(const QStringList &declarationImport){
-
+    //Считать компоненты импорта пустыми
+    QString componentsImport = "";
+    //Если
+    if(!declarationImport.isEmpty()){
+        //Для каждого компонента, кроме последнего
+        for(int i =1;i<declarationImport.size()-2;++i){
+            //Добавить компонент в строку и добавить точку после него
+            componentsImport.append(declarationImport[i]);
+            componentsImport.append(".");
+        }
+        //Добавить последний компонент в строку
+        componentsImport.append(declarationImport[declarationImport.size()-2]);
+    }
+    //Добавить строку с имортом в пакет
+    return componentsImport;
 }
 
 //Разбить метод
 method splitMethod(const QStringList &code, int &indexCurrentString, int &indexCurrentSimbol, const QString &nameClass, const QStringList &methodDeclaration, QSet<error> errors){
-
+    //Запоминаем начало метода
+    const int startCodeMethodString = indexCurrentString, startCodeMethodSimbol = indexCurrentSimbol-1;
+    //Считаь что количество открывающих скоб 1 закрывающих 0
+    int countOpenBracket = 1;
+    int countCloseBracket = 0;
+    QString pattern = "\\/\\*|\\\"|\\'|\\/\\/|\\}|\\{";
+    QRegularExpression modifierRegex(pattern);
+    //Пока текущая строка меньше строки с завершением лексем
+    QStringList codeMethod;
+    if(methodDeclaration.last() == "{"){
+        while(indexCurrentString<code.size() && countOpenBracket!=countCloseBracket) {
+            //Найти слова
+            QRegularExpressionMatch match = modifierRegex.match(code[indexCurrentString].mid(indexCurrentSimbol));
+            //Если нашли слово
+            if (match.hasMatch()) {
+                QString foundWord =match.captured();
+                indexCurrentSimbol = indexCurrentSimbol +match.capturedEnd();
+                if(foundWord == "/*"){
+                    skipMultilineComment(code, indexCurrentString, indexCurrentSimbol);
+                }
+                else if(foundWord == "\""){
+                    skipConstant(code[indexCurrentString],indexCurrentSimbol, '"');
+                }
+                else if(foundWord == "'"){
+                    skipConstant(code[indexCurrentString],indexCurrentSimbol, '\'');
+                }
+                else if(foundWord == "//"){
+                    indexCurrentString++;
+                    indexCurrentSimbol=0;
+                }
+                else if(foundWord == "{"){
+                    countOpenBracket++;
+                }
+                else{
+                    countCloseBracket++;
+                }
+            }
+            //Иначе перейти на следующую строку
+            else{
+                indexCurrentString++;
+                indexCurrentSimbol = 0;
+            }
+        }
+        codeMethod.append(code[startCodeMethodString].mid(startCodeMethodSimbol-1));
+        codeMethod.append(code.mid(startCodeMethodString+1, (indexCurrentString-1)-(startCodeMethodString)));
+        codeMethod.append(code[indexCurrentString].mid(0, indexCurrentSimbol));
+    }
+    QString nameMethod;
+    QString mod;
+    //Найти индекс начала ( с аргументами
+    int indexStartArg = methodDeclaration.indexOf("(");
+    //Найти имя метода(перед скобкой с аргументами)
+    nameMethod = methodDeclaration[indexStartArg-1];
+    //Если имя не совпадает с именем класса
+    //Получить строки до ( это объявление
+    //Получить строку в ) это аргументы
+    QStringList stringBeforeArg = methodDeclaration.mid(0, indexStartArg);
+    QStringList stringArg = methodDeclaration.mid(indexStartArg+1, methodDeclaration.size() -indexStartArg-3 );
+    //Если в объявлении есть public
+    //Считать что модификатор доступа public
+    if(stringBeforeArg.contains("public"))  mod = "Public";
+    //Иначе если в объявлении есть private
+    //Считать что модификатор доступа private
+    else if(stringBeforeArg.contains("private")) mod = "Private";
+    //Иначе если в объявлении есть protected
+    //Считать что модификатор доступа protected
+    else if(stringBeforeArg.contains("protected"))  mod = "Protected";
+    //Иначе
+    //Считать что модификатор доступа private
+    else  mod = "default";
+    QVector<argument> arguments;
+    //Если список аргументов не пуст
+    if(!stringArg.empty()){
+        argument arg;
+        int i=-1;
+        do{
+            i++;
+            arg.setType(stringArg[i]);
+            i++;
+            arg.setName(stringArg[i]);
+            i++;
+            arguments.append(arg);
+        }while(i < stringArg.size() && stringArg[i] == ",");
+    }
+    QString filename;
+    filename.append("(");
+    if(arguments.isEmpty() == false){
+        for(int i = 0;i< arguments.size()-1;++i){
+            filename.append(arguments[i].getType());
+            filename.append(" ,");
+        }
+        filename.append(arguments[arguments.size()-1].getType());
+    }
+    filename.append(")");
+    method currentMethod(mod, filename, codeMethod, arguments);
+    if(nameMethod != nameClass){
+        currentMethod.setNameMethod(nameMethod);
+        if(stringBeforeArg.contains("abstract"))  currentMethod.setIsAbstract(true);
+        else if(stringBeforeArg.contains("static")) currentMethod.setIsStatic(true);
+        currentMethod.setReturnType(stringBeforeArg[stringBeforeArg.size()-2]);
+        currentMethod.setFilename(currentMethod.getFilename().prepend(nameMethod));
+    }
+    else{
+        constructor currentConstructor(mod, filename, codeMethod, arguments);
+        currentMethod.setFilename(currentMethod.getFilename().prepend(nameClass));
+    }
+    return currentMethod;
 }
 
 // Разбить интерфейс
@@ -190,7 +313,30 @@ interface_info splitInterface(const QStringList &code, int &indexCurrentString, 
 
 // Разбить поле
 QMap<QString, field> splitField(const QStringList &fieldDeclaration){
-
+    QMap<QString, field> arrayFields;
+    field currentField;
+    int indexType=1;
+    if(fieldDeclaration.contains("static")) {
+        currentField.setIsStatic(true);
+        indexType++;
+    }
+    if(fieldDeclaration.contains("public"))  currentField.setMod("Public");
+    else if(fieldDeclaration.contains("private")) currentField.setMod("Private");
+    else if(fieldDeclaration.contains("protected"))  currentField.setMod("Protected");
+    else{
+        indexType--;
+        currentField.setMod("Private");
+    }
+    currentField.setType(fieldDeclaration[indexType]);
+    indexType++;
+    while(fieldDeclaration.mid(indexType).contains(",") == true){
+        currentField.setNameField(fieldDeclaration[indexType]);
+        arrayFields.insert(fieldDeclaration[indexType], currentField);
+        indexType = fieldDeclaration.mid(indexType).indexOf(",")+indexType+1;
+    }
+    currentField.setNameField(fieldDeclaration[indexType]);
+    arrayFields.insert(fieldDeclaration[indexType], currentField);
+    return arrayFields;
 }
 
 // Разбить класс
