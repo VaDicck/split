794d0498a70e30caa83097e3ea1fa907f27eb710
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-20 17:46:42 +0300

    final

--- testsplitfield.cpp
+++ testsplitfield.cpp
@@ -3,7 +3,7 @@
 #include <QMap>
 
 
-void testSplitField::add_data() {
+void testSplitField::test_field_data() {
     QTest::addColumn<QStringList>("fieldDeclaration");
     QTest::addColumn<fieldMap>("expectedResult");
 
@@ -39,7 +39,7 @@ void testSplitField::add_data() {
     QTest::newRow("static_no_modifier")
         << QStringList({ "static", "int", "a", ";"})
         << fieldMap({
-               {"a", field("a", "int", "default", true)}
+               {"a", field("a", "int", "default", true)},
            });
 
     // Тест 6: Статическое перед модификатором
@@ -101,33 +101,12 @@ void testSplitField::add_data() {
            });
 }
 
-void testSplitField::add() {
+void testSplitField::test_field() {
     QFETCH(QStringList, fieldDeclaration);
     QFETCH(fieldMap, expectedResult);
 
     fieldMap actualResult = splitField(fieldDeclaration);
-
-    if (actualResult != expectedResult) {
-        // Найдем лист ключей
-        QStringList masKeyActual = actualResult.keys();
-        QStringList masKeyExtend = expectedResult.keys();
-        // Проверим на количество полей
-        QVERIFY2(masKeyActual==masKeyExtend , qPrintable(QString("\nРазное количество полей: ожидалось (%1) {%2}, получено (%3) {%4}")
-                                       .arg(expectedResult.size()).arg(masKeyExtend.join(", "))
-                                       .arg(actualResult.size()).arg(masKeyActual.join(", "))));
-
-        int minLen = qMin(masKeyActual.size(), masKeyExtend.size());
-        for(int i =0;i<minLen;++i){
-            QVERIFY2(actualResult.value(masKeyActual[i]).getIsStatic() == expectedResult.value(masKeyExtend[i]).getIsStatic(), qPrintable(QString("\nЯвляется ли поле статическим: ожидалось (%1), получено (%2)")
-            .arg(expectedResult.value(masKeyExtend[i]).getIsStatic()).arg(actualResult.value(masKeyActual[i]).getIsStatic())));
-            QVERIFY2(actualResult.value(masKeyActual[i]).getNameField() == expectedResult.value(masKeyExtend[i]).getNameField(), qPrintable(QString("\nИмя поля: ожидалось (%1), получено (%2)")
-            .arg(expectedResult.value(masKeyExtend[i]).getNameField()).arg(actualResult.value(masKeyActual[i]).getNameField())));
-            QVERIFY2(actualResult.value(masKeyActual[i]).getType() == expectedResult.value(masKeyExtend[i]).getType(), qPrintable(QString("\nТип поля: ожидалось (%1), получено (%2)")
-            .arg(expectedResult.value(masKeyExtend[i]).getType()).arg(actualResult.value(masKeyActual[i]).getType())));
-            QVERIFY2(actualResult.value(masKeyActual[i]).getMod() == expectedResult.value(masKeyExtend[i]).getMod(), qPrintable(QString("\nМодификатор доступа поля: ожидалось (%1), получено (%2)")
-            .arg(expectedResult.value(masKeyExtend[i]).getMod()).arg(actualResult.value(masKeyActual[i]).getMod())));
-        }
-    }
+    verifyMapField(actualResult, expectedResult);
 }
 
 
