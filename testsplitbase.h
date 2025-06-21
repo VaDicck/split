badf1116ef607be4d856bd6b7a5d1d34eb099012
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-22 01:18:02 +0300

    Добавлена проверка пакетов и все функции возвраащают сообщение с ошибками

--- testsplitbase.h
+++ testsplitbase.h
@@ -8,18 +8,20 @@
 #include "class_info.h"
 #include "interface_info.h"
 #include "method.h"
+#include "package_info.h"
 
 
 class testSplitBase : public QObject
 {
     Q_OBJECT
 protected:
-    void verifyMapField(const QMap<QString, field>& actual,const QMap<QString, field>& expected);
+    QString verifyMapField(const QMap<QString, field>& actual,const QMap<QString, field>& expected);
     void verifyErrors(const QSet<error>&actual, const QSet<error>&expected);
-    void verifyConstructor(const constructor& actual,const constructor& expected);
-    void verifyMethod(const method& actual,const method& expected);
-    void verifyClass(const class_info& actual,const class_info& expected);
-    void verifyInterface(const interface_info& actual,const interface_info& expected);
+    QString verifyConstructor(const constructor& actual,const constructor& expected);
+    QString verifyMethod(const method& actual,const method& expected);
+    QString verifyClass(const class_info& actual,const class_info& expected);
+    QString verifyInterface(const interface_info& actual,const interface_info& expected);
+    QString verifyPack(const package_info& actual,const package_info& expected, QString pathPack);
 };
 
 #endif // TESTSPLITBASE_H
