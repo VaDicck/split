fe23e66a5aa7323b04b9ac6243f9d87d2ab7de81
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-24 02:13:55 +0300

    Добавлены комментарии в стиле Doxygen

--- testsplitproject.h
+++ testsplitproject.h
@@ -1,3 +1,10 @@
+/*!
+ * \file testsplitproject.h
+ * \brief Заголовочный файл класса testsplitproject
+ * \file testsplitproject.cpp
+ * \brief Реализация тестов для splitProject
+ */
+
 #ifndef TESTSPLITPROJECT_H
 #define TESTSPLITPROJECT_H
 
@@ -9,12 +16,26 @@
 #include "class_info.h"
 #include "interface_info.h"
 
-
+/*!
+ * \class testsplitproject
+ * \ingroup TestClasses
+ * \brief Класс для тестирования разбора проектов
+ *
+ * Класс содержит тесты для проверки корректности работы функции splitProject,
+ * которая обрабатывает весь проект, включая пакеты, классы и интерфейсы
+ */
 class testsplitproject : public testSplitBase
 {
     Q_OBJECT
 private slots:
+    /*!
+     * \brief Подготавливает тестовые данные для проверки разбора проектов
+     */
     void test_pack_data();
+
+    /*!
+     * \brief Тестирует функцию splitProject
+     */
     void test_pack();
 };
 #endif // TESTSPLITPROJECT_H
