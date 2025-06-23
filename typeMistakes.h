c02667c8b05b1a411cb57647f8e0b9b3d4ac86b0
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-24 00:45:27 +0300

    Добавлены комментарии в стиле Doxygen

--- typeMistakes.h
+++ typeMistakes.h
@@ -1,23 +1,38 @@
+/*!
+ * \file typemistakes.h
+ * \brief Заголовочный файл перечисления typeMistakes
+ *
+ * Содержит перечисление возможных типов ошибок:
+ */
+
 #ifndef TYPEMISTAKES_H
 #define TYPEMISTAKES_H
+
+/**
+ * \enum typeMistakes
+ * \brief Перечисление типов возможных ошибок в коде
+ *
+ * Определяет все возможные типы ошибок, которые могут быть обнаружены
+ */
 enum class typeMistakes {
-    inputFileDoesNotExist,    // входного файла не существует
-    inputJavaFileDoesNotExist,// входного Java файла не существует
-    manyStrokes,             // много строк
-    inputFileEmpty,          // входной файл пуст
-    outputFileDoesNotExist,   // выходного файла не существует
-    manyJavaFiles,           // превышает допустимое количество Java файлов
-    fileWithoutPackage,      // файл без явного указания пакета
-    manyClassesInFile,       // превышает допустимое количество классов
-    manyInterfacesInFile,    // превышает допустимое количество интерфейсов
-    manyMethodInClass,       // превышает допустимое количество методов в классе
-    manyMethodInInterface,   // превышает допустимое количество методов в интерфейсе
-    manyFieldInClass,        // превышает допустимое количество полей в классе
-    manyFieldInInterface,    // превышает допустимое количество полей в интерфейсе
-    noClosingFiguredScoop,   // нет закрывающей фигурной скобы
-    noOpenningFiguredScoop,  // нет открывающей фигурной скобы
-    notClosedComment,        // не закрыт комментарий
-    notClosedSingleQuotes,   // не закрыты одинарные кавычки
-    notClosedDoubleQuotes    // не закрыты двойные кавычки
+    inputFileDoesNotExist,    ///< Входного файла не существует
+    inputJavaFileDoesNotExist,///< Входного Java файла не существует
+    manyStrokes,             ///< Превышено допустимое количество строк в файле
+    inputFileEmpty,          ///< Входной файл пуст
+    outputFileDoesNotExist,  ///< Выходного файла не существует
+    manyJavaFiles,           ///< Превышено допустимое количество Java файлов
+    fileWithoutPackage,      ///< Файл без явного указания пакета
+    manyClassesInFile,       ///< Превышено допустимое количество классов в файле
+    manyInterfacesInFile,    ///< Превышено допустимое количество интерфейсов в файле
+    manyMethodInClass,       ///< Превышено допустимое количество методов в классе
+    manyMethodInInterface,   ///< Превышено допустимое количество методов в интерфейсе
+    manyFieldInClass,        ///< Превышено допустимое количество полей в классе
+    manyFieldInInterface,    ///< Превышено допустимое количество полей в интерфейсе
+    noClosingFiguredScoop,   ///< Нет закрывающей фигурной скобки
+    noOpenningFiguredScoop,  ///< Нет открывающей фигурной скобки
+    notClosedComment,        ///< Не закрыт комментарий
+    notClosedSingleQuotes,   ///< Не закрыты одинарные кавычки
+    notClosedDoubleQuotes    ///< Не закрыты двойные кавычки
 };
+
 #endif // TYPEMISTAKES_H
