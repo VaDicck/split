ce5cb138da1511e6d1e80c6b28bc82c5061fe0b6
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-24 02:02:36 +0300

    Добавлены комментарии в стиле Doxygen

--- testsplitimport.cpp
+++ testsplitimport.cpp
@@ -4,19 +4,29 @@ void testsplitimport::test_split_import_data() {
     QTest::addColumn<QStringList>("importDeclaration");
     QTest::addColumn<QString>("expectedResult");
 
-    // Типичный импорт
+    /*!
+     * \test Тест 1: Типичный импорт
+     * Проверяет разбор стандартного импорта пакета
+     */
     QTest::newRow("typical_import")
-            << QStringList({{"import"}, {"java"}, {"util"}, {"List"}, {";"}})
-            << QString("java.util.List");
+        << QStringList({{"import"}, {"java"}, {"util"}, {"List"}, {";"}})
+        << QString("java.util.List");
 
-    // Добавить все пакеты
+    /*!
+     * \test Тест 2: Импорт всех пакетов
+     * Проверяет разбор импорта с (*)
+     */
     QTest::newRow("add_all_package")
-            << QStringList({{"import"}, {"java"}, {"util"}, {"List"}, {"*"}, {";"}})
-            << QString("java.util.List.*");
-    // Один пакет
+        << QStringList({{"import"}, {"java"}, {"util"}, {"List"}, {"*"}, {";"}})
+        << QString("java.util.List.*");
+
+    /*!
+     * \test Тест 3: Один пакет
+     * Проверяет разбор импорта одного пакета
+     */
     QTest::newRow("one_package")
-            << QStringList({{"import"}, {"java"}, {";"}})
-            << QString("java");
+        << QStringList({{"import"}, {"java"}, {";"}})
+        << QString("java");
 }
 
 void testsplitimport::test_split_import() {
