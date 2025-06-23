f7da09fc269987df61bdebc966e9042706653a89
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-24 02:10:40 +0300

    Добавлены комментарии в стиле Doxygen.

--- testsplitmethod.h
+++ testsplitmethod.h
@@ -1,3 +1,10 @@
+/*!
+ * \file testsplitmethod.h
+ * \brief Заголовочный файл класса testsplitmethod
+ * \file testsplitmethod.cpp
+ * \brief Реализация тестов для testsplitmethod
+ */
+
 #ifndef TESTSPLITMETHOD_H
 #define TESTSPLITMETHOD_H
 
@@ -8,11 +15,23 @@
 #include "main.h"
 #include "error.h"
 
+/*!
+ * \class testsplitmethod
+ * \ingroup TestClasses
+ * \brief Класс для тестирования разбора методов
+ */
 class testsplitmethod : public testSplitBase
 {
     Q_OBJECT
 private slots:
+    /*!
+     * \brief Подготавливает тестовые данные для проверки разбора методов
+     */
     void test_method_data();
+
+    /*!
+     * \brief Тестирует функцию splitMethod
+     */
     void test_method();
 };
 
