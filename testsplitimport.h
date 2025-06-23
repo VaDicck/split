9427341da4040c91d9b5c3f362a059e2f7080184
VaDicck <vadiknadezhkin@yandex.ru>
2025-06-24 02:02:44 +0300

    Добавлены комментарии в стиле Doxygen

--- testsplitimport.h
+++ testsplitimport.h
@@ -1,3 +1,10 @@
+/*!
+ * \file testsplitimport.h
+ * \brief Заголовочный файл класса testsplitimport
+ * \file testsplitimport.cpp
+ * \brief Реализация тестов для функции splitImport
+ */
+
 #ifndef TESTSPLITIMPORT_H
 #define TESTSPLITIMPORT_H
 
@@ -5,12 +12,23 @@
 #include <QTest>
 #include <main.h>
 
-
+/*!
+ * \class testsplitimport
+ * \brief Класс для тестирования разбиения(сборки) import-выражений
+ */
 class testsplitimport : public QObject
 {
     Q_OBJECT
 private slots:
+    /*!
+     * \brief Подготавливает тестовые данные для проверки разбора import
+     */
     void test_split_import_data();
+
+    /*!
+     * \brief Тестирует функцию splitImport
+     */
     void test_split_import();
 };
+
 #endif // TESTSPLITIMPORT_H
